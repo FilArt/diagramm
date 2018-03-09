af302b66a2e4fe663301d2c3efbecdcddfd98fe7
filart97 <filart97@gmail.com>
2018-03-09 13:50:28 +0300

    Initial commit

---
+++ diagramm.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'diagramm.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(172, 120)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 10, 151, 81))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.gridLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.lineEdit = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lineEdit)
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.SpanningRole, self.pushButton)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.lineEdit_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "a"))
+        self.label_2.setText(_translate("MainWindow", "k"))
+        self.pushButton.setText(_translate("MainWindow", "Построить диаграмму"))
+
+        self.pushButton.clicked.connect(self.plot)
+
+    def plot(self):
+        import pyqtgraph as pg
+        plot = pg.plot()
+        plot.setAspectLocked()
+
+        # diagramm
+        import numpy as np
+        import scipy.special as sp
+
+        a = int(self.lineEdit.text())
+        k = int(self.lineEdit_2.text())
+
+        # make polar data
+        theta = np.linspace(0.000001, 2 * np.pi, 10000)
+        arg = k * a * np.sin(theta)
+        radius = 2 * sp.jvp(0, arg) / arg
+
+        # Transform to cartesian and plot
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+
+        # Add polar grid lines
+        plot.addLine(x=0, pen=0.2)
+        plot.addLine(y=0, pen=0.2)
+        for r in [0.25, 0.5, 1]:
+            circle = pg.QtGui.QGraphicsEllipseItem(-r, -r, r * 2, r * 2)
+            circle.setPen(pg.mkPen(0.15))
+            plot.addItem(circle)
+
+        plot.plot(y, abs(x))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
